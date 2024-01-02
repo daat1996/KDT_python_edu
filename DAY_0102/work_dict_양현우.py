16221351d83eac85faff5cdd4b67bedac38a46c2
daat <tempest1996@gmail.com>
2024-01-02 19:50:45 +0900

    심사문제 완료

--- DAY_0102/work_dict_양현우.py
+++ DAY_0102/work_dict_양현우.py
@@ -51,3 +51,53 @@ print(tuple(a))
 
 b = (4,5,6)
 print(list(b))
+
+a, b, c = [1,2,3]
+print(a,b,c)
+
+a,b,c = (1,2,3)
+print(a,b,c)
+
+print(type(a))
+
+# 딕셔너리 사용
+
+lux = {'health': 490, 'mana':334, 'melee':550, 'armor':18.72}
+
+print(lux)
+
+lux = {'health': 490, 'health': 800, 'mana':334, 'melee':550, 'armor':18.72}
+print(lux)  # 키가 중복되면 가장 마지막에 있는 값 사용..
+
+lux1 = dict(health=490, mana=334, melee=550, armor=18.72)
+print(lux1)
+
+# (키, 값) 형식의 튜플로 딕셔너리를 만듦
+
+print('health'in lux1)
+print('attack' in lux1)
+
+x = dict({'a':10, 'b':20})
+print(x)
+print(type({'a':10, 'b':20}))
+
+# 12.4 연습문제
+camille = { 'health' : 575.6,
+            'health_regen' : 1.7,
+            'mana': 338.8,
+            'mana_regen':1.63,
+            'melee': 125,
+            'attack_damage': 60,
+            'attack_speed': 0.625,
+            'armor': 26,
+            'magic_resistance':32.1,
+            'movement_speed': 340}
+print(camille['health'])
+print(camille['movement_speed'])
+
+#12.5 심사문제
+
+key = input().split()
+value = input().split()
+print(dict(zip(key,value)))     # zip 써야하나..
+
