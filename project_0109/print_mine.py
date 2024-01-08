c032667550e2a1236b61a8c080d9e8d0a76cc263
daat <tempest1996@gmail.com>
2024-01-08 19:34:01 +0900

    1월3일 오전

--- project_0109/print_mine.py
+++ project_0109/print_mine.py
@@ -1,7 +1,25 @@
 from random import randint, sample
 
-list1 = ['*','0']
+
+def makeMine():
+    matrix = []
+    for sero in range(9):
+        matseries = []
+        for garo in range(9):
+            num = randint(1, 8)
+            if num%8 == 0:
+                matseries.append('*')
+            else:
+                matseries.append('0')
+        matrix.append(matseries)
+    return matrix
+
+
 for a in range(9):
     for b in range(9):
-        print(sample(list1,1),end=''if b<8 else '\n')
+        if randint(1,8)%8 == 0:
+            print('*', end=''if b<8 else '\n')
+        else:
+            print('0', end=''if b<8 else '\n')
 
+print(makeMine())
